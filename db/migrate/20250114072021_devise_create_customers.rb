b1dfd8ddd4810b62222c7d68a39ffeb3677eef37
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2025-01-14 07:43:15 +0000

    [add]devise

--- db/migrate/20250114072021_devise_create_customers.rb
+++ db/migrate/20250114072021_devise_create_customers.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[6.1]
+class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
   def change
-    create_table :users do |t|
+    create_table :customers do |t|
       ## Database authenticatable
+      t.string :name, null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -13,7 +14,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
 
       ## Rememberable
       t.datetime :remember_created_at
-
+      
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
@@ -36,9 +37,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :customers, :email,                unique: true
+    add_index :customers, :reset_password_token, unique: true
+    # add_index :customers, :confirmation_token,   unique: true
+    # add_index :customers, :unlock_token,         unique: true
   end
 end
