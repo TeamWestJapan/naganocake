e47111f1361523572f1d18bd2f25a9876f6384f4
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-01-16 11:16:08 +0000

    customer orderpage

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,12 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!, only: [:new, :create]
+
   def new
+    @order = Order.new
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = "{current_customer.last_name} #{current_customer.first_name}"
+    @addresses = current_customer.addresses
   end
 
   def thanks
@@ -9,11 +16,33 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def confirm
+    @order = Order.new(order_params)
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.customer = current_customer
+    case params[:order][:shipping_address_type]
+    when 'own_address'
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = "#{current_customer.last_name} #{current_customer.first_name}"
+    when 'new_address'
+      @order.postal_code = params[:order][:shipping_postal_code]
+      @order.address = params[:order][:shipping_address]
+      @order.name = params[:order][:shipping_name]
+    end
+    @order.save
+      redirect_to confirm_orders_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :address)
   end
 end
