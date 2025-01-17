a69f2e90bed37251a0ff13da9884b80fdff5d6cd
sukizumi <187249056+sukizumi@users.noreply.github.com>
2025-01-17 05:55:53 +0000

    admin/item, admin/genre completed

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,32 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item, notice: '商品を登録しました'
+    else
+      render 'new'
+    end
+  end
+
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+
 end
