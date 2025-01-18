eaa22069045a2510d43b8408c5b79f309e24e3d0
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-01-18 03:39:47 +0000

    order index show

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,9 @@ class Address < ApplicationRecord
     validates :address, presence: true
     validates :name, presence: true
     belongs_to :customer
+
+    def address_display
+      '〒' + postal_code + ' ' + address + ' ' + name
+    end
+
 end
