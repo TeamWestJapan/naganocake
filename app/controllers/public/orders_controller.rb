a60fcb757bd69d5119ea8b8f180b1bb74a7bdbd5
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-01-19 15:24:14 +0000

    [add]order/new.confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -22,7 +22,31 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
+    @cart_items = CartItem.where(customer_id: current_customer.id)
     @shipping_cost = 800
+    #ary = []
+    #@cart_items.each do |cart_item|
+      #ary <<cart_item.item.price*cart_item.quantity
+    #end
+    #@cart_items_price = ary.sum
+    @address_type = params[:order][:address_type]
+    case @address_type
+    when "own_address"  # 自身の住所を使う場合
+      @order_address = current_customer.postal_code + " " + current_customer.address + " " + current_customer.last_name + current_customer.first_name
+    when "saved_address"  # 保存済みの住所を使う場合
+      if params[:order][:saved_address_id].present?
+        selected = Address.find(params[:order][:saved_address_id])
+        @selected_address = selected.postal_code + " " + selected.address + " " + selected.name
+      else
+        render :new  # 保存済み住所が選択されていない場合は新規注文画面に戻す
+      end
+    when "new_address"  # 新しい住所を使う場合
+      @new_postal_code = params[:order][:new_postal_code]
+      @new_address = params[:order][:new_address]
+      @new_name = params[:order][:new_name]
+    else
+      render :new 
+    end
   end
 
   def create
