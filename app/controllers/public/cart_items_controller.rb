ec9dd9d258d12c2e305b371cc5051c39e7eec8b7
sukizumi <187249056+sukizumi@users.noreply.github.com>
2025-01-20 06:24:45 +0000

    cart almost finished

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,20 +1,34 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items
   end
 
   def update
   end
 
   def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to request.referer
   end
 
   def destroy_all
+    cart_item = CartItem.all
+    cart_item.destroy_all
+    redirect_to request.referer
   end
 
   def create
-    @item = Item.find(cart_item_params[:item_id])
-    if @item.save
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.customer_id = current_customer.id
+    cart_item.item_id = cart_item_params[:item_id]
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.update(amount: cart_item.amount)
+      redirect_to cart_items_path
+    else
+      cart_item.save
       redirect_to cart_items_path
     end
   end
