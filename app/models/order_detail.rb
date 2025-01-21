e6453f49089d2ab87e6b04a708f541878b575b00
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-01-21 06:23:54 +0000

    order page

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,7 +1,7 @@
 class OrderDetail < ApplicationRecord
     validates :price, presence: true
     validates :amount, presence: true
-    validates :is_status, presence: true
+    validates :making_status, presence: true
     belongs_to :order
     belongs_to :item
 end
