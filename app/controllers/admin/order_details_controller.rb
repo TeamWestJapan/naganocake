d2788d6e26c7e27dfb004252df372a009e9842a6
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2025-01-21 06:06:38 +0000

    [add]detail

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,20 +1,6 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    @order_detail = OrderDetail.find(params[:order.id][:id])
-    @detail_status = [:order][:order_detail][:detail_status]
-    case @detail_status
-    when "製作不可"
-      @order_detail.making_status = 0
-    when "製作待ち"
-      @order_detail.making_status = 1
-    when "製作中"
-      @order_detail.making_status = 2
-    else "製作完了"
-      @order_detail.making_status = 3
-    end
-    @order_detail.update(order_detail_params)
-    redirect_to request.referer
-  end
+    @order_detail = OrderDetail.find(params[:order_id])
 
   private
 
