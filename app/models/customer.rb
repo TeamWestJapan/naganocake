ffcc65aaa7b5c6b482c2b6f609e70d8bb8ea5bf2
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2025-01-22 08:21:40 +0000

    [add]rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,4 +9,12 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   
   validates :is_active, inclusion: [true, false]
+
+  def full_name
+    last_name + " " + first_name
+  end
+
+  def full_name_kana
+    last_name_kana + " " + first_name_kana
+  end
 end
