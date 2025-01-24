e7a85221712ee5d730c2cd281c229871893aef72
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-01-24 09:55:58 +0000

    admin order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,16 +3,25 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @order_details = OrderDetail.where(order_id: @order.id)
+    @order_details = @order.order_details
   end
 
 
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    if @order.status == "payment_confirm"
+      @order.order_details.update_all(making_status: "waiting_for_production")
+    end
+    if @order.update(order_params)
+      if @order.order_details.where(making_status: "produced").count == @order.order_details.count
+        # すべて "produced" なら Order の status を "preparing_for_shipping" に更新
+        @order.update(status: "preparing_for_shipping")
+      end
+    end
     redirect_to request.referer
   end
-
+    
   private
 
   def order_params
