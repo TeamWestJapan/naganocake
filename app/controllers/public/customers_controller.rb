df7c0849fed9d6d4d6bb48b0d6309838d00bdd01
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2025-01-24 03:41:31 +0000

    [add]before

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,5 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!, only: [:show, :edit, :update, :unsubscribe, :withdraw]
   def show
     @customer = current_customer
   end
